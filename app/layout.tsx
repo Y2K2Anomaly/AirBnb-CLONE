0e415fc0f84ea792816c236d3e7e477047b91fc8
Pawan Maurya <y2k2anomaly@gmail.com>
2023-10-28 01:12:20 +0530

    added filters feature

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ import RentModal from './components/modals/RentModal';
 
 import ToasterProvider from './providers/ToasterProvider';
 import getCurrentUser from './actions/getCurrentUser';
+import SearchModal from './components/modals/SearchModal';
 
 const font = Nunito({ subsets: ['latin'] })
 
@@ -34,6 +35,7 @@ export default async function RootLayout({
         <RegisterModal />
         <LoginModal />
         <RentModal />
+        <SearchModal />
         <Navbar currentUser={currentUser} />
         <div className='pb-20 pt-28'>
           {children}
